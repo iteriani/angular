8aed98bbc273d311a2e86e052d276a1815f8b8ab
Thomas Nguyen <tomnguyen@google.com>
2024-04-11 13:11:02 -0700

    feat(core): Fix license

--- packages/core/primitives/event-dispatch/src/event_type.ts
+++ packages/core/primitives/event-dispatch/src/event_type.ts
@@ -1,6 +1,9 @@
 /**
  * @license
- * SPDX-License-Identifier: MIT
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
  */
 
 /*
